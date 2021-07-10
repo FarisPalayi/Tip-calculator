0a6ac595c7f362cc7860f1f7dae043f05fda7dbb
FarisPalayi <fariswritecode@gmail.com>
2021-07-10 18:13:25 +0530

    📜 first draft script

--- Script/index.js
+++ Script/index.js
@@ -1,39 +1,27 @@
 var billInput = document.querySelector('input[name=bill]');
 var billPercentBtns = document.querySelectorAll('.card__section__btns-wrapper__btn');
 var tipAmountElm = document.querySelector('.card__section__container--tip .card__section__container__amount');
-var totalBillElm = document.querySelector('.card__section__container--total card__section__container__amount');
+var totalBillElm = document.querySelector('.card__section__container--total .card__section__container__amount');
 var peopleInput = document.querySelector('input[name=people]');
-var bill = 0;
+var bill = billInput.value;
 var tipPercent = 0;
-var people = 0;
-var totalBill = 0;
-var tipAmount = 0;
-function updateTipPercent() {
-    billPercentBtns.forEach(function (percentBtn) {
-        percentBtn.addEventListener('click', function () {
-            tipPercent = parseInt(percentBtn.getAttribute('data-percent'));
-            tipAmount = tipPercent / 100 * bill;
-            tipAmountElm.innerHTML = tipAmount.toFixed(2);
-        });
+var people = peopleInput.value;
+billPercentBtns.forEach(function (percentBtn) {
+    percentBtn.addEventListener('click', function () {
+        tipPercent = parseInt(percentBtn.getAttribute('data-percent'));
+        showTipPerson(calculateTipPerPerson(bill, tipPercent));
+        showTotalBill(calculateTotalBillPerPerson(bill, people));
     });
+});
+function showTipPerson(tipAmount) {
+    tipAmountElm.innerHTML = tipAmount;
 }
-updateTipPercent();
-updateBill();
-updatePeople();
-updateTotalBill();
-updateTipAmount();
-function updateBill() {
-    bill = parseInt(billInput.value);
+function calculateTipPerPerson(bill, tipPercent) {
+    return bill * tipPercent / 100;
 }
-function updatePeople() {
-    people = parseInt(peopleInput.value);
+function showTotalBill(totalBill) {
+    totalBillElm.innerHTML = totalBill;
 }
-function updateTotalBill() {
-    totalBill = bill * people;
+function calculateTotalBillPerPerson(bill, people) {
+    return bill * people;
 }
-function updateTipAmount() {
-    tipAmount = totalBill * tipPercent / 100;
-    tipAmountElm.innerHTML = tipAmount.toFixed(2);
-}
-// const perPerson = percent / people;
-// function updateTotal
--- Script/index.ts
+++ Script/index.ts
@@ -1,48 +1,33 @@
 const billInput = document.querySelector('input[name=bill]') as HTMLInputElement;
 const billPercentBtns = document.querySelectorAll('.card__section__btns-wrapper__btn');
 const tipAmountElm = document.querySelector('.card__section__container--tip .card__section__container__amount') as HTMLSpanElement;
-const totalBillElm = document.querySelector('.card__section__container--total card__section__container__amount') as HTMLSpanElement;
+const totalBillElm = document.querySelector('.card__section__container--total .card__section__container__amount') as HTMLSpanElement;
 const peopleInput = document.querySelector('input[name=people]') as HTMLInputElement;
 
-let bill = 0;
+let bill = billInput.value;
 let tipPercent = 0;
-let people = 0;
+let people = peopleInput.value;
 
-let totalBill = 0;
-let tipAmount = 0;
+billPercentBtns.forEach((percentBtn) => {
+percentBtn.addEventListener('click', () => {
+    tipPercent = parseInt(percentBtn.getAttribute('data-percent'));
+    showTipPerson(calculateTipPerPerson(bill, tipPercent));
+    showTotalBill(calculateTotalBillPerPerson(bill, people));
+});
+});
 
-function updateTipPercent() {
-  billPercentBtns.forEach((percentBtn) => {
-    percentBtn.addEventListener('click', () => {
-      tipPercent = parseInt(percentBtn.getAttribute('data-percent'));
-      tipAmount = tipPercent / 100 * bill;
-      tipAmountElm.innerHTML = tipAmount.toFixed(2);
-    });
-  });
-}
-updateTipPercent();
-updateBill();
-updatePeople();
-updateTotalBill();
-updateTipAmount();
-
-function updateBill() {
-    bill = parseInt(billInput.value);
+function showTipPerson(tipAmount) {
+    tipAmountElm.innerHTML = tipAmount;
 }
 
-function updatePeople() {
-    people = parseInt(peopleInput.value);
+function calculateTipPerPerson(bill, tipPercent) {
+    return bill * tipPercent / 100;
 }
 
-function updateTotalBill() {
-    totalBill = bill * people;
+function showTotalBill(totalBill) {
+    totalBillElm.innerHTML = totalBill;
 }
 
-function updateTipAmount() {
-    tipAmount = totalBill * tipPercent / 100;
-    tipAmountElm.innerHTML = tipAmount.toFixed(2);
+function calculateTotalBillPerPerson(bill, people) {
+    return bill * people;
 }
-
-
-// const perPerson = percent / people;
-// function updateTotal
