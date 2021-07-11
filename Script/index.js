2ddea35870990c2d49fc8e7e0a7a7a7d31bd906b
FarisPalayi <fariswritecode@gmail.com>
2021-07-11 12:24:42 +0530

    ✔ add simple validation

--- Script/index.js
+++ Script/index.js
@@ -1,8 +1,10 @@
 /**
  * @author Faris P
  * @license MIT
- * @description basic tip calculator
+ * @description a basic tip calculator
  */
+// -------------------------
+// elements
 var billInput = document.querySelector('input[name=bill]');
 var billPercentBtns = document.querySelectorAll('.card__section__btns-wrapper__btn');
 var tipAmountElm = document.querySelector('.card__section__container--tip .card__section__container__amount');
@@ -10,50 +12,28 @@ var totalBillElm = document.querySelector('.card__section__container--total .car
 var peopleInput = document.querySelector('input[name=people]');
 var customTipPercentInput = document.querySelector('input[name=custom]');
 // -------------------------
+// vars
 var bill = parseFloat(billInput.value) || 0;
-var tipInPercent = 0;
-var people = parseInt(peopleInput.value) || 0;
-billPercentBtns === null || billPercentBtns === void 0 ? void 0 : billPercentBtns.forEach(function (percentBtn) {
-    percentBtn.addEventListener('click', function (event) {
-        updateTipPercent(event);
-        showTipPerPerson();
-        showTotalBillPerPerson();
-        removeBtnActive();
-        setBtnActive(percentBtn, true);
-    });
-});
-function updateCustomInputDataPercent(event) {
+var tipInPercent = .15; // initial value
+var people = parseInt(peopleInput.value) || 1;
+// -------------------------
+// updagte variable values
+function updateBill() {
+    bill = parseFloat(billInput.value) || 0;
 }
 function updateTipPercent(event) {
     var _a;
     var clickedElmPercent = (_a = event.target) === null || _a === void 0 ? void 0 : _a.getAttribute('data-percent');
     tipInPercent = parseFloat(clickedElmPercent);
 }
-function setBtnActive(element, active) {
-    var btnActiveClass = 'card__section__btns-wrapper__btn--active';
-    element.classList.toggle(btnActiveClass, active);
+function updateCustomTipPercent() {
+    tipInPercent = parseFloat(customTipPercentInput.value) || 0;
 }
-function removeBtnActive() {
-    billPercentBtns === null || billPercentBtns === void 0 ? void 0 : billPercentBtns.forEach(function (percentBtn) {
-        setBtnActive(percentBtn, false);
-    });
-}
-billInput === null || billInput === void 0 ? void 0 : billInput.addEventListener('input', function () {
-    updateBill();
-    showTipPerPerson();
-    showTotalBillPerPerson();
-});
-function updateBill() {
-    bill = parseFloat(billInput.value) || 50;
-}
-peopleInput === null || peopleInput === void 0 ? void 0 : peopleInput.addEventListener('input', function () {
-    updatePeople();
-    showTipPerPerson();
-    showTotalBillPerPerson();
-});
 function updatePeople() {
     people = parseInt(peopleInput.value) || 0;
 }
+// -------------------------
+// 
 function calculateTip() {
     return bill * tipInPercent / 100;
 }
@@ -64,8 +44,11 @@ function prependDollarSign(amount) {
     return "$" + amount.toFixed(2);
 }
 function showTipPerPerson() {
-    tipAmountElm.innerHTML = prependDollarSign(calculateTipPerPerson());
+    tipAmountElm.innerHTML = prependDollarSign(calculateTipPerPerson()) || '$0.00';
 }
+showTipPerPerson();
+// -------------------------
+// 
 function calculateTotalBill() {
     return bill + calculateTip();
 }
@@ -73,8 +56,80 @@ function calculateTotalBillPerPerson() {
     return calculateTotalBill() / people;
 }
 function showTotalBillPerPerson() {
-    totalBillElm.innerHTML = prependDollarSign(calculateTotalBillPerPerson());
+    totalBillElm.innerHTML = prependDollarSign(calculateTotalBillPerPerson()) || '$0.00';
 }
-showTipPerPerson();
 showTotalBillPerPerson();
 // -------------------------
+function setBtnActive(element, active) {
+    var btnActiveClass = 'card__section__btns-wrapper__btn--active';
+    element.classList.toggle(btnActiveClass, active);
+}
+function removeBtnActive() {
+    billPercentBtns === null || billPercentBtns === void 0 ? void 0 : billPercentBtns.forEach(function (percentBtn) {
+        setBtnActive(percentBtn, false);
+    });
+}
+// -------------------------
+// 
+billInput === null || billInput === void 0 ? void 0 : billInput.addEventListener('input', function () {
+    updateBill();
+    showTipPerPerson();
+    showTotalBillPerPerson();
+});
+billPercentBtns === null || billPercentBtns === void 0 ? void 0 : billPercentBtns.forEach(function (percentBtn) {
+    percentBtn.addEventListener('click', function (event) {
+        updateTipPercent(event);
+        showTipPerPerson();
+        showTotalBillPerPerson();
+        removeBtnActive();
+        setBtnActive(percentBtn, true);
+    });
+});
+customTipPercentInput === null || customTipPercentInput === void 0 ? void 0 : customTipPercentInput.addEventListener('input', function () {
+    updateCustomTipPercent();
+    showTipPerPerson();
+    showTotalBillPerPerson();
+    removeBtnActive();
+});
+peopleInput === null || peopleInput === void 0 ? void 0 : peopleInput.addEventListener('input', function () {
+    updatePeople();
+    showTipPerPerson();
+    showTotalBillPerPerson();
+    validate();
+});
+// -------------------------
+// resets the inputs
+var reset = document.querySelector('.card__section__reset-btn');
+reset === null || reset === void 0 ? void 0 : reset.addEventListener('click', resetInputs);
+function resetInputs() {
+    billInput.value = '0';
+    peopleInput.value = '0';
+    customTipPercentInput.value = '0';
+    tipAmountElm.innerHTML = '$0.00';
+    totalBillElm.innerHTML = '$0.00';
+}
+// -------------------------
+// validation
+var errorElm = document.querySelector('.card__section__error');
+function isValidInputs() {
+    var peopleInputAsANumber = parseInt(peopleInput.value);
+    return !isNaN(peopleInputAsANumber) && peopleInputAsANumber > 0;
+}
+function showError() {
+    errorElm === null || errorElm === void 0 ? void 0 : errorElm.setAttribute('aria-hidden', 'false');
+    errorElm.classList.add('card__section__error--visible');
+}
+function hideError() {
+    errorElm.setAttribute('aria-hidden', 'true');
+    errorElm.classList.remove('card__section__error--visible');
+}
+function validate() {
+    if (isValidInputs()) {
+        hideError();
+        console.log(true);
+    }
+    else {
+        showError();
+        console.log(false);
+    }
+}
--- Script/index.ts
+++ Script/index.ts
@@ -1,33 +1,31 @@
 /**
  * @author Faris P
  * @license MIT
- * @description basic tip calculator
+ * @description a basic tip calculator
  */
 
+// -------------------------
+// elements
+
 const billInput: HTMLInputElement | null = document.querySelector('input[name=bill]');
-const billPercentBtns: NodeListOf<Element> | null = document.querySelectorAll('.card__section__btns-wrapper__btn');
+const billPercentBtns: NodeListOf<HTMLButtonElement> | null = document.querySelectorAll('.card__section__btns-wrapper__btn');
 const tipAmountElm: HTMLSpanElement | null = document.querySelector('.card__section__container--tip .card__section__container__amount');
 const totalBillElm: HTMLSpanElement | null = document.querySelector('.card__section__container--total .card__section__container__amount');
 const peopleInput: HTMLInputElement | null = document.querySelector('input[name=people]');
 const customTipPercentInput: HTMLInputElement | null = document.querySelector('input[name=custom]');
 
 // -------------------------
+// vars
 
 let bill: number = parseFloat(billInput!.value) || 0;
-let tipInPercent: number = 0;
-let people: number = parseInt(peopleInput!.value) || 0;
+let tipInPercent: number = .15; // initial value
+let people: number = parseInt(peopleInput!.value) || 1;
 
-billPercentBtns?.forEach((percentBtn: Element): void => {
-  percentBtn.addEventListener('click', (event: Event): void => {
-    updateTipPercent(event);
-    showTipPerPerson();
-    showTotalBillPerPerson();
-    removeBtnActive();
-    setBtnActive(percentBtn, true);
-  });
-});
+// -------------------------
+// updagte variable values
 
-function updateCustomInputDataPercent(event: Event): void {
+function updateBill(): void {
+  bill = parseFloat(billInput!.value) || 0;
 }
 
 function updateTipPercent(event: Event): void {
@@ -35,36 +33,16 @@ function updateTipPercent(event: Event): void {
   tipInPercent = parseFloat(clickedElmPercent!);
 }
 
-function setBtnActive(element: Element, active: boolean): void {
-  const btnActiveClass = 'card__section__btns-wrapper__btn--active'
-  element.classList.toggle(btnActiveClass, active);
+function updateCustomTipPercent(): void {
+  tipInPercent = parseFloat(customTipPercentInput!.value) || 0;
 }
-
-function removeBtnActive(): void {
-  billPercentBtns?.forEach((percentBtn: Element): void => {
-    setBtnActive(percentBtn, false);
-  });
-}
-
-billInput?.addEventListener('input', (): void => {
-  updateBill();
-  showTipPerPerson();
-  showTotalBillPerPerson();
-});
-
-function updateBill(): void {
-  bill = parseFloat(billInput!.value) || 50;
-}
-
-peopleInput?.addEventListener('input', (): void => {
-  updatePeople();
-  showTipPerPerson();
-  showTotalBillPerPerson();
-});
 
 function updatePeople(): void {
   people = parseInt(peopleInput!.value) || 0;
 }
+
+// -------------------------
+// 
 
 function calculateTip(): number {
   return bill * tipInPercent / 100;
@@ -79,8 +57,13 @@ function prependDollarSign(amount: number): string {
 }
 
 function showTipPerPerson(): void {
-  tipAmountElm!.innerHTML = prependDollarSign(calculateTipPerPerson());
+  tipAmountElm!.innerHTML = prependDollarSign(calculateTipPerPerson()) || '$0.00';
 }
+
+showTipPerPerson();
+
+// -------------------------
+// 
 
 function calculateTotalBill(): number {
   return bill + calculateTip();
@@ -91,10 +74,98 @@ function calculateTotalBillPerPerson(): number {
 }
 
 function showTotalBillPerPerson(): void {
-  totalBillElm!.innerHTML = prependDollarSign(calculateTotalBillPerPerson());
+  totalBillElm!.innerHTML = prependDollarSign(calculateTotalBillPerPerson()) || '$0.00';
 }
 
-showTipPerPerson();
 showTotalBillPerPerson();
 
 // -------------------------
+
+function setBtnActive(element: Element, active: boolean): void {
+  const btnActiveClass = 'card__section__btns-wrapper__btn--active'
+  element.classList.toggle(btnActiveClass, active);
+}
+
+function removeBtnActive(): void {
+  billPercentBtns?.forEach((percentBtn: Element): void => {
+    setBtnActive(percentBtn, false);
+  });
+}
+
+// -------------------------
+// 
+
+billInput?.addEventListener('input', (): void => {
+  updateBill();
+  showTipPerPerson();
+  showTotalBillPerPerson();
+});
+
+billPercentBtns?.forEach((percentBtn: HTMLButtonElement): void => {
+  percentBtn.addEventListener('click', (event: Event): void => {
+    updateTipPercent(event);
+    showTipPerPerson();
+    showTotalBillPerPerson();
+    removeBtnActive();
+    setBtnActive(percentBtn, true);
+  });
+});
+
+customTipPercentInput?.addEventListener('input', (): void => {
+  updateCustomTipPercent();
+  showTipPerPerson();
+  showTotalBillPerPerson();
+  removeBtnActive();
+});
+
+peopleInput?.addEventListener('input', (): void => {
+  updatePeople();
+  showTipPerPerson();
+  showTotalBillPerPerson();
+  validate();
+});
+
+// -------------------------
+// resets the inputs
+
+const reset: HTMLButtonElement | null = document.querySelector('.card__section__reset-btn');
+
+reset?.addEventListener('click', resetInputs);
+
+function resetInputs(): void {
+  billInput!.value = '0';
+  peopleInput!.value = '0';
+  customTipPercentInput!.value = '0';
+  tipAmountElm!.innerHTML = '$0.00';
+  totalBillElm!.innerHTML = '$0.00';
+}
+
+// -------------------------
+// validation
+
+const errorElm: HTMLSpanElement | null = document.querySelector('.card__section__error');
+
+function isValidInputs(): boolean {
+  const peopleInputAsANumber: number = parseInt(peopleInput!.value);
+  return !isNaN(peopleInputAsANumber) && peopleInputAsANumber > 0;
+}
+
+function showError(): void {
+  errorElm?.setAttribute('aria-hidden', 'false');
+  errorElm!.classList.add('card__section__error--visible');
+}
+
+function hideError(): void {
+  errorElm!.setAttribute('aria-hidden', 'true');
+  errorElm!.classList.remove('card__section__error--visible');
+}
+
+function validate(): void {
+  if (isValidInputs()) {
+    hideError();
+    console.log(true)
+  } else {
+    showError();
+    console.log(false)
+  }
+}
